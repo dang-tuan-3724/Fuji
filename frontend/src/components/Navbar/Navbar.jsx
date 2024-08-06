bb86a27a8962ef187f73c50e227a6d7c71c6eaaa
dang-tuan-3724 <tuan.dangminh372004@hcmut.edu.vn>
2024-08-06 22:36:39 +0700

    Initial

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+
+const Navbar = () => {
+    const [menu, setMenu] = useState("Home")
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt="" className="logo" />
+        <ul className="navbar-menu">
+            <li className={menu==="Home"?"active":""}>Home</li>
+            <li className={menu==="Member"?"active":""}>Member</li>
+            <li className={menu==="Check"?"active":""}>Check</li>
+            <li className={menu==="Chart"?"active":""}>Chart</li>
+        </ul>
+        <div className="navbar-right">
+            <button>Login</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

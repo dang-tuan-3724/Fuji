f84e132d6f9ecb9a61922384b189889c272408d9
dang-tuan-3724 <tuan.dangminh372004@hcmut.edu.vn>
2024-09-12 12:23:27 +0700

    tuan dep trai sieu cap vu tru

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://tuandangminh372004:0788778027AbcD@clusterfwb.64pgc.mongodb.net/FUJI').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://tuandangminh372004:0788778027AbcD@clusterfwb.64pgc.mongodb.net/fuji').then(()=>console.log("DB Connected"));
 }

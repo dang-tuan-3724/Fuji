713950d210e7b48da50003132a8356093a20b0a0
dang-tuan-3724 <tuan.dangminh372004@hcmut.edu.vn>
2024-09-22 22:48:41 +0700

    h

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,5 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+export const url = 'https://fuji-bmap.onrender.com'
+// export const url = 'http://localhost:4000'
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,8 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) =>{
     
-    const url = "http://localhost:4000";
+    const url = "https://fuji-bmap.onrender.com";
+    // const url = "http://localhost:4000";
     const [token,setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
